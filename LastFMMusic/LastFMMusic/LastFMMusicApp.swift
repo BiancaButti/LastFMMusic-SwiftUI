13e6d241ec36571b2dd64df4e8752d1ff26c8f6f
Bianca Butti <72713090+BiancaButti@users.noreply.github.com>
2025-06-01 18:49:14 -0300

    Remove ignored files and apply .gitignore

---
+++ LastFMMusic/LastFMMusic/LastFMMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  LastFMMusicApp.swift
+//  LastFMMusic
+//
+//  Created by Bianca on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LastFMMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
